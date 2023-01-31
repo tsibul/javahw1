5fe69201d1afe7bedd0c1739a34171abb3e3cbba
Ilya Tsibul <tsigel@vikivostok.ru>
2023-02-01 01:50:52 +0300

    finished HW2

--- src/HW2.java
+++ src/HW2.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
@@ -7,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.logging.*;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -28,7 +27,7 @@ public class HW2 {
     }
 
     public static HashMap resFields (){
-        HashMap <String, String> res = new HashMap <String, String>();
+        HashMap <String, String> res = new HashMap<>();
         res.put("предмет", " по предмету ");
         res.put("оценка", " получил ");
         res.put("фамилия", "Студент ");
@@ -42,21 +41,102 @@ public class HW2 {
 
     public static void main (String[] args) throws IOException, IllegalAccessException {
         ///task 1
+            /*
+            1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder.
+            Данные для фильтрации приведены ниже в виде json строки.
+            Если значение null, то параметр не должен попадать в запрос.
+            Параметры для фильтрации: String str = "{'name':'Ivanov', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
+            Результат "select * from students where “name” = “Ivanov” and “country”=”Russia” …
+             */
+        /*
+        System.out.println("Task 1");
         String fileName = "sql_rq.json";
         StringBuilder sqlRq = sqlReq(fileName, 0);
         System.out.println(sqlRq);
+        System.out.println();
+         */
         /// task2
+        System.out.println("Task 2");
+        Logger iLogger = Log("logbubble.txt");
+        System.out.println("input array length");
+        int len = iScanner.nextInt();
+        System.out.println("input array range");
+        int range = iScanner.nextInt();
+        Integer [] array = randomArr(len, range);
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array, iLogger);
+        System.out.println(Arrays.toString(array));
+            /*
+            2) Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+             */
         /// task3
+            /*
+            3) ** Дана json строка (можно сохранить в файл и читать из файла)
+            [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+            {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+            {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+            Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+            Пример вывода:
+            Студент Иванов получил 5 по предмету Математика.
+            Студент Петрова получил 4 по предмету Информатика.
+            Студент Краснов получил 5 по предмету Физика.
+             */
         String resFile = "students.json";
         JSONArray stList = arrayFromJsonFile(resFile);
         StringBuilder studRes = txtFromStudents(stList);
         System.out.println(studRes);
         /// task4
-        calc();
+            /*
+            4) К калькулятору из предыдущего дз добавить логирование.
+            */
+        Logger calcLogger = Log("logcalc.txt");
+        calc(calcLogger);
+        iScanner.close();
+
+    }
+
+    public static Logger Log (String logfile) throws IOException {
+        Logger iLogger = Logger.getLogger(HW2.class.getName());
+        iLogger.setUseParentHandlers(false);
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("\\src\\" + logfile);
+        FileHandler fh = new FileHandler(pathFile);
+        iLogger.addHandler(fh);
+        SimpleFormatter txt = new SimpleFormatter();
+        fh.setFormatter(txt);
+        return iLogger;
+    }
+
+    public  static void bubbleSort (Integer[] list, Logger iLogger){
+        int n = list.length;
+        Integer i = 0;
+        boolean changed = true;
+        while (changed){
+            changed = false;
+            for (Integer j = 0; j < n - i - 1; j++){
+                if(list[j] > list[j + 1]){
+                    int tmp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = tmp;
+                    iLogger.log(Level.INFO,  "swap element with index " + j.toString());
+                    changed = true;
+                }
+            }
+            i++;
+            iLogger.log(Level.INFO, "finished step " + i.toString());
+        }
+    }
+    public static Integer[] randomArr (int len, int range ) {
+        Random random = new Random();
+        Integer [] arr = new Integer[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = random.nextInt(range);
+        }
+        return arr;
     }
 
     public static StringBuilder txtFromStudents(JSONArray list){
-        StringBuilder txtRes = new StringBuilder("");
+        StringBuilder txtRes = new StringBuilder();
         for (Object stResult: list) {
             Map<String, Object> studentResult = ((JSONObject) stResult).toMap();
             studentResult = sortByKeys(studentResult);
@@ -76,7 +156,7 @@ public class HW2 {
     }
 
     public static StringBuilder studentToString(Student student) throws IllegalAccessException {
-        StringBuilder txt = new StringBuilder("");
+        StringBuilder txt = new StringBuilder();
         Field [] stFields = student.getClass().getDeclaredFields();
         for(Field field: stFields){
             field.setAccessible(true);
@@ -98,7 +178,7 @@ public class HW2 {
 
     public static ArrayList<Student> studentsParse(String file) throws IOException {
         JSONArray jsonArray = arrayFromJsonFile(file);
-        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Student> students = new ArrayList<>();
         Student tmp = new Student();
         for(Object i :jsonArray) {
             tmp.country = (String) ((JSONObject) i).toMap().get("country");
@@ -110,7 +190,7 @@ public class HW2 {
         return students;
     }
 
-    public static void calc()  {
+    public static void calc(Logger logger)  {
         boolean stop = false;
         while (!stop) {
             Integer inp1 = null;
@@ -120,18 +200,23 @@ public class HW2 {
                 System.out.print("input number 1 ");
                 try {
                     inp1 = iScanner.nextInt();
+                    logger.log(Level.INFO, "input number 1 " + inp1.toString());
                     err = false;
                 } catch (Exception ex){
+                    logger.log(Level.INFO, "wrong input number 1 ");
                     System.out.print("wrong input");
                     System.out.println();
+                    iScanner.next();
                 }}
             err = true;
             while(err) {
                 System.out.print("input number 2 ");
                 try {
                     inp2 = iScanner.nextInt();
+                    logger.log(Level.INFO, "input nomber 2 " + inp1.toString());
                     err = false;
                 } catch (Exception ex){
+                    logger.log(Level.INFO, "wrong input number 2 ");
                     System.out.print("wrong input");
                     System.out.println();
                     iScanner.next();
@@ -140,6 +225,7 @@ public class HW2 {
             while(err) {
                 System.out.print("input action or stop ");
                 String act = iScanner.next();
+                logger.log(Level.INFO, "input act " + act.toString());
                 switch (act) {
                     case "+" -> {
                         System.out.println();
@@ -172,6 +258,7 @@ public class HW2 {
                         stop = true;
                     }
                     default -> {
+                        logger.log(Level.INFO, "wrong input act");
                         System.out.println("wrong input");
                         iScanner.next();
                     }
@@ -179,7 +266,6 @@ public class HW2 {
             }
         }
     }
-
 
     public static <K extends Comparable, V> Map<K, V> sortByKeys(Map<K, V> map)
     {
@@ -193,26 +279,3 @@ public class HW2 {
         return treeMap;
     }
 }
-
-
-/*
-1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder.
-Данные для фильтрации приведены ниже в виде json строки.
-Если значение null, то параметр не должен попадать в запрос.
-Параметры для фильтрации: String str = "{'name':'Ivanov', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
-Результат "select * from students where “name” = “Ivanov” and “country”=”Russia” …
-
-2) Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
-
-3) ** Дана json строка (можно сохранить в файл и читать из файла)
-[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
-{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
-{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
-Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
-Пример вывода:
-Студент Иванов получил 5 по предмету Математика.
-Студент Петрова получил 4 по предмету Информатика.
-Студент Краснов получил 5 по предмету Физика.
-
-4) К калькулятору из предыдущего дз добавить логирование.
- */
